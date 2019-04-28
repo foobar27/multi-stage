d87f9ecfcce9d06e1bde5e0ad2e9a4074df548fb
Sébastien Wagener <sebastien.wagener@gmail.com>
2019-04-28 19:18:39 +0200

    Move and fix clojure parser test

--- test/multi_stage/clojure/parser_test.clj
+++ test/multi_stage/clojure/parser_test.clj
@@ -1,51 +1,51 @@
-(ns multi-stage.clojure-parser-test
+(ns multi-stage.clojure.parser-test
   (:require [clojure.test :refer :all]
             [clojure.walk :refer [macroexpand-all]]
             [multi-stage.test-utils :refer [remove-auto-gensym]]
-            [multi-stage.clojure-parser :refer :all]))
+            [multi-stage.clojure.parser :refer :all]))
 
 (deftest function-argument-parsing
   (are [expected input]
       (= expected
-         (destructure-fn* (rest (remove-auto-gensym (macroexpand-all input)))))
+         (destructure-clj `fn* (rest (remove-auto-gensym (macroexpand-all input)))))
 
     ;; fn, 0 args
     {:name nil
      :arities {0 {:args []
-                  :body [1]}}}
+                  :bodies [1]}}}
     `(fn [] 1)
 
     {:name 'foo
      :arities {0 {:args []
-                  :body [1]}}}
+                  :bodies [1]}}}
     `(fn ~'foo [] 1)
 
     ;; fn, 1 args
     {:name nil
      :arities {1 {:args ['x]
-                  :body [`(+ ~'x 1)]}}}
+                  :bodies [`(+ ~'x 1)]}}}
     `(fn [~'x] (+ ~'x 1))
 
     {:name 'foo
      :arities {1 {:args ['x]
-                  :body [`(+ ~'x 1)]}}}
+                  :bodies [`(+ ~'x 1)]}}}
     `(fn ~'foo [~'x] (+ ~'x 1))
 
     ;; fn, 2 arities
     {:name nil
      :arities {1 {:args ['x]
-                  :body [`(+ ~'x 1)]}
+                  :bodies [`(+ ~'x 1)]}
                2 {:args ['x 'y]
-                  :body [`(+ ~'x ~'y)]}}}
+                  :bodies [`(+ ~'x ~'y)]}}}
     `(fn
        ([~'x] (+ ~'x 1))
        ([~'x ~'y] (+ ~'x ~'y)))
 
     {:name 'foo
      :arities {1 {:args ['x]
-                  :body [`(+ ~'x 1)]}
+                  :bodies [`(+ ~'x 1)]}
                2 {:args ['x 'y]
-                  :body [`(+ ~'x ~'y)]}}}
+                  :bodies [`(+ ~'x ~'y)]}}}
     `(fn ~'foo
        ([~'x] (+ ~'x 1))
        ([~'x ~'y] (+ ~'x ~'y)))
@@ -53,19 +53,19 @@
     ;; reader macro, 0 args
     {:name nil
      :arities {0 {:args []
-                  :body ['(42)]}}}
+                  :bodies ['(42)]}}}
     `#(42)
     
     ;; reader macro, 1 arg
     {:name nil
      :arities {1 {:args ['p1]
-                  :body [`(+ ~'p1 1)]}}}
+                  :bodies [`(+ ~'p1 1)]}}}
     `#(+ % 1)
 
     ;; reader macro, 2 args
     {:name nil
      :arities {2 {:args ['p1 'p2]
-                  :body [`(+ ~'p1 ~'p2)]}}}
+                  :bodies [`(+ ~'p1 ~'p2)]}}}
     `#(+ %1 %2))
   
   ;; TODO what about docstrings, pre- and post-conditions?
