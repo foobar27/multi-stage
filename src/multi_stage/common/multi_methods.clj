b4db0dc09331e55112486d4e8bfcd4b00f3177f7
Sébastien Wagener <sebastien.wagener@gmail.com>
2020-08-15 17:59:16 +0200

    Make determine-best-method return a value-or-exception

--- src/multi_stage/common/multi_methods.clj
+++ src/multi_stage/common/multi_methods.clj
@@ -43,23 +43,28 @@
                     (dominates? multi-method hierarchy (first entry) (first best-entry)))
               entry
               best-entry))
-          (improve-and-verify [best-entry entry]
-            (let [best-entry (improve best-entry entry)]
-              (if (dominates? multi-method hierarchy (first best-entry) (first entry))
-                best-entry
-                ;; TODO do not throw the exception, return it.
-                (throw (IllegalArgumentException. (str "Multiple methods in multimethod " name
-                                                       "match dispatch value" dispatch-val "->"
-                                                       (first entry) "and" (first best-entry)))))))]
+          (improve-and-verify [best-entry-or-exception entry]
+            (condp = (first best-entry-or-exception)
+              ::exception best-entry-or-exception ;; propagate exception
+              ::value (let [best-entry (second best-entry-or-exception)
+                            best-entry (improve best-entry entry)]
+                        (if (dominates? multi-method hierarchy (first best-entry) (first entry))
+                          [::value best-entry]
+                          [::exception (str "Multiple methods in multimethod " name
+                                            "match dispatch value" dispatch-val "->"
+                                            (first entry) "and" (first best-entry))]))))]
     (reduce improve-and-verify
-            nil
+            [::value nil]
             (filter #(h/isa? hierarchy dispatch-val (first %)) method-table))))
 
 (defn get-method [{:keys [::method-table ::default-dispatch-value] :as multi-method} hierarchy dispatch-val]
-  (let [best-entry (determine-best-entry multi-method hierarchy dispatch-val)]
-    (if (nil? best-entry)
-      (get method-table default-dispatch-value)
-      (second best-entry))))
+  (let [value-or-exception (determine-best-entry multi-method hierarchy dispatch-val)]
+    (if (= ::value (first value-or-exception))
+      (let [best-entry (second value-or-exception)]
+        (if (nil? best-entry)
+          (get method-table default-dispatch-value)
+          (second best-entry)))
+      (throw (IllegalArgumentException. (second value-or-exception))))))
 
 (defn- determine-candidates [hierarchy dispatch-val]
   (into #{dispatch-val}
