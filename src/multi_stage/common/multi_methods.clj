1b9dfa39907a9b1b5f103beb1b5b9777af81fab0
Sébastien Wagener <sebastien.wagener@gmail.com>
2020-08-15 17:33:08 +0200

    Decouple hierarchy atom as separate argument

--- src/multi_stage/common/multi_methods.clj
+++ src/multi_stage/common/multi_methods.clj
@@ -2,11 +2,8 @@
   (:refer-clojure :exclude [defmulti defmethod remove-method prefer-method get-method methods prefers remove-all-methods])
   (:require [multi-stage.common.hierarchy :as h]))
 
-;; TODO spec that hierarchy is an atom!
-(defn make-multi-method [name dispatch-fn hierarchy default-dispatch-value]
+(defn make-multi-method [name dispatch-fn default-dispatch-value]
   {::name name
-   ::dispatch-fn dispatch-fn
-   ::hierarchy hierarchy
    ::default-dispatch-value default-dispatch-value
    ::method-table {}
    ::prefer-table {}})
@@ -19,21 +16,18 @@
           (fn [t]
             (dissoc t dispatch-val))))
 
-(defn- prefers? [{:keys [::prefer-table ::hierarchy] :as multi-method} x y]
+(defn- prefers? [{:keys [::prefer-table] :as multi-method} hierarchy x y]
   (let [x-prefs (get prefer-table x)]
     (or (contains? x-prefs y)
-        (let [hierarchy @hierarchy]
-          (some #(prefers? multi-method x %) (h/parents hierarchy y))
-          (some #(prefers? multi-method % y) (h/parents hierarchy x))))))
+        (some #(prefers? multi-method hierarchy x %) (h/parents hierarchy y))
+        (some #(prefers? multi-method hierarchy % y) (h/parents hierarchy x)))))
 
-(defn- dominates? [multi-method x y]
-  (or (prefers? multi-method x y)
-      (h/isa? @(::hierarchy multi-method) x y)))
+(defn- dominates? [multi-method hierarchy x y]
+  (or (prefers? multi-method hierarchy x y)
+      (h/isa? hierarchy x y)))
 
-clojure.core/prefer-method
-
-(defn prefer-method [multi-method dispatch-val-x dispatch-val-y]
-  (if (prefers? multi-method dispatch-val-y dispatch-val-x)
+(defn prefer-method [multi-method hierarchy dispatch-val-x dispatch-val-y]
+  (if (prefers? multi-method hierarchy dispatch-val-y dispatch-val-x)
     (throw (IllegalStateException. (str "Preference conflict in multi-method " (::name multi-method) ": "
                                         dispatch-val-y " is already preferred to " dispatch-val-x)))
     (update-in multi-method
@@ -43,27 +37,26 @@ clojure.core/prefer-method
 (defn prefers [multi-method]
   (get multi-method ::prefer-table))
 
-(defn- determine-best-entry [{:keys [::hierarchy ::method-table] :as multi-method} dispatch-val]
-  (let [hierarchy @hierarchy]
-    (letfn [(improve [best-entry entry]
-              (if (or (nil? best-entry)
-                      (dominates? multi-method (first entry) (first best-entry)))
-                entry
-                best-entry))
-            (improve-and-verify [best-entry entry]
-              (let [best-entry (improve best-entry entry)]
-                (if (dominates? multi-method (first best-entry) (first entry))
-                  best-entry
-                  ;; TODO do not throw the exception, return it.
-                  (throw (IllegalArgumentException. (str "Multiple methods in multimethod " name
-                                                         "match dispatch value" dispatch-val "->"
-                                                         (first entry) "and" (first best-entry)))))))]
-      (reduce improve-and-verify
-              nil
-              (filter #(h/isa? hierarchy dispatch-val (first %)) method-table)))))
+(defn- determine-best-entry [{:keys [::method-table] :as multi-method} hierarchy dispatch-val]
+  (letfn [(improve [best-entry entry]
+            (if (or (nil? best-entry)
+                    (dominates? multi-method hierarchy (first entry) (first best-entry)))
+              entry
+              best-entry))
+          (improve-and-verify [best-entry entry]
+            (let [best-entry (improve best-entry entry)]
+              (if (dominates? multi-method hierarchy (first best-entry) (first entry))
+                best-entry
+                ;; TODO do not throw the exception, return it.
+                (throw (IllegalArgumentException. (str "Multiple methods in multimethod " name
+                                                       "match dispatch value" dispatch-val "->"
+                                                       (first entry) "and" (first best-entry)))))))]
+    (reduce improve-and-verify
+            nil
+            (filter #(h/isa? hierarchy dispatch-val (first %)) method-table))))
 
-(defn get-method [{:keys [::hierarchy ::method-table ::default-dispatch-value] :as multi-method} dispatch-val]
-  (let [best-entry (determine-best-entry multi-method dispatch-val)]
+(defn get-method [{:keys [::method-table ::default-dispatch-value] :as multi-method} hierarchy dispatch-val]
+  (let [best-entry (determine-best-entry multi-method hierarchy dispatch-val)]
     (if (nil? best-entry)
       (get method-table default-dispatch-value)
       (second best-entry))))
@@ -73,9 +66,8 @@ clojure.core/prefer-method
         (h/descendants hierarchy dispatch-val)))
 
 (comment
-  (defn group-methods [multi-method]
-    (let [hierarchy @(::hierarchy multi-method)
-          method-table (::method-table multi-method)
+  (defn group-methods [multi-method hierarchy]
+    (let [method-table (::method-table multi-method)
           candidate-keys (into #{}
                                (mapcat #(determine-candidates hierarchy)
                                        (keys method-table)))
--- test/multi_stage/common/multi_methods_test.clj
+++ test/multi_stage/common/multi_methods_test.clj
@@ -4,20 +4,20 @@
             [clojure.test :refer [deftest testing are is]]))
 
 (deftest basic-multi-method-test
-  (let [too-simple (-> (mm/make-multi-method 'too-simple
-                                             identity
-                                             (atom (h/make-hierarchy))
-                                             :default)
+  (let [hierarchy (mm/make-multi-method 'too-simple
+                                        identity
+                                        :default)
+        too-simple (-> hierarchy
                        (mm/add-method ::a ::a-fn)
                        (mm/add-method ::b ::b-fn)
                        (mm/add-method :default ::default-fn))]
     (testing "Check basic dispatch"
       (are [dispatch-val the-fn]
-          (= the-fn (mm/get-method too-simple dispatch-val))
+          (= the-fn (mm/get-method too-simple hierarchy dispatch-val))
         ::a ::a-fn
         ::b ::b-fn))
     (testing "Remove a method"
-      (is (= ::default-fn (mm/get-method too-simple ::unknown))))))
+      (is (= ::default-fn (mm/get-method too-simple hierarchy ::unknown))))))
 
 (deftest isa?-multi-method-test
   (let [hierarchy (-> (h/make-hierarchy)
@@ -29,13 +29,12 @@
                       (h/derive ::parabola ::conic-section))
         shapes (-> (mm/make-multi-method 'shapes
                                          identity
-                                         (atom hierarchy)
                                          :default)
                    (mm/add-method ::rectangle ::rectangle-fn)
                    (mm/add-method ::ellipse ::ellipse-fn)
                    (mm/add-method :default ::default-fn))]
     (are [dispatch-val the-fn]
-        (= the-fn (mm/get-method shapes dispatch-val))
+        (= the-fn (mm/get-method shapes hierarchy dispatch-val))
       ::rectangle     ::rectangle-fn
       ::square        ::rectangle-fn
       ::ellipse       ::ellipse-fn
@@ -48,15 +47,14 @@
                       (h/derive ::rectangle ::shape))
         bar (-> (mm/make-multi-method 'bar
                                       identity
-                                      (atom hierarchy)
                                       :default)
                 (mm/add-method [::rectangle ::shape] ::rectangle-shape-fn)
                 (mm/add-method [::shape ::rectangle] ::shape-rectangle-fn))]
     (testing "In case of a conflict, an exception should be thrown"
       (is (thrown? java.lang.IllegalArgumentException
-                   (mm/get-method bar [::rectangle ::rectangle]))))
+                   (mm/get-method bar hierarchy [::rectangle ::rectangle]))))
     (testing "The prefers method returns empty table"
       (is (= {} (mm/prefers bar))))
-    (let [bar (mm/prefer-method bar [::rectangle ::shape] [::shape ::rectangle])]
+    (let [bar (mm/prefer-method bar hierarchy [::rectangle ::shape] [::shape ::rectangle])]
       (testing "Adding a preference to resolve it dispatches correctly"
-        (is (= ::rectangle-shape-fn (mm/get-method bar [::rectangle ::rectangle])))))))
+        (is (= ::rectangle-shape-fn (mm/get-method bar hierarchy [::rectangle ::rectangle])))))))
